de1b9477d8466d9c0eef38a980ecf93017c79912
Baraa Khamaysa <engbaraakhamaysa@gmail.com>
2026-09-07 08:51:13 +0300

    feat: add OpenRouter API token

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+
+api_key = os.environ.get("OPENROUTER_API_KEY")
+
+if api_key is None:
+    raise RuntimeError("OPENROUTER_API_KEY is not set")
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+
 def main():
-    print("Hello from aiagent!")
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=[
+            {
+                "role": "user",
+                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+            }
+        ],
+    )
+
+    print(response.choices[0].message.content)
 
 
 if __name__ == "__main__":
