afe1871d880adea5aaccb8da0ab13e0d234576c3
Baraa Khamaysa <engbaraakhamaysa@gmail.com>
2026-09-07 10:51:37 +0300

    feat: add token metadata

--- main.py
+++ main.py
@@ -16,16 +16,27 @@ client = OpenAI(
 
 
 def main():
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+
     response = client.chat.completions.create(
         model="openrouter/free",
         messages=[
             {
                 "role": "user",
-                "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+                "content": prompt,
             }
         ],
     )
 
+    if response.usage is None:
+     raise RuntimeError("Response usage is missing")
+
+    print(f"User prompt: {prompt}")
+
+    print(f"Prompt tokens: {response.usage.prompt_tokens}")
+    print(f"Response tokens: {response.usage.completion_tokens}")
+
+    print("Response:")
     print(response.choices[0].message.content)
 
 
