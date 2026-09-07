e3e4a0d5057dae8d625d425bb20471fce8d3f8d2
Baraa Khamaysa <engbaraakhamaysa@gmail.com>
2026-09-07 17:12:45 +0300

    feat: add multiple messages

--- main.py
+++ main.py
@@ -22,16 +22,13 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     args = parser.parse_args()
 
-
-
+    messages = [
+       {"role":"user", "content":args.user_prompt}
+    ]
+    
     response = client.chat.completions.create(
         model="openrouter/free",
-        messages=[
-            {
-                "role": "user",
-                "content": args.user_prompt,
-            }
-        ],
+        messages=messages,
     )
 
     if response.usage is None:
